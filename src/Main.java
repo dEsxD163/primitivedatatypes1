3bee5de5a9c4981bf568870a012726ec0bf25c9e
Denis <dzavalskiy@gmail.com>
2023-03-07 15:48:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 20000; // стоимость билета
+        int y = 20; // стоимость для начисления 1 мили
+        System.out.println("Колличество миль с одного билета:" + (x / y));
+    }
+
+}
