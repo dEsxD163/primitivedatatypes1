abd6ffba03445329b4c1fa17ff9ea36a490fa398
Denis <dzavalskiy@gmail.com>
2023-03-09 14:35:25 +0400

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_price = 20000; // стоимость билета
-        int mile_cost = 20; // стоимость для начисления 1 мили
-        System.out.println("Колличество миль с одного билета:" + (ticket_price / mile_cost));
+        int ticketPrice = 20000; // стоимость билета
+        int mileCost = 20; // стоимость для начисления 1 мили
+        System.out.println("Колличество миль с одного билета:" + (ticketPrice / mileCost));
     }
 
 }
